9803953cb431b28d62c3e5a76e6e302a63e2ac0a
Rajsi Sinha <rajsisinha03@gmail.com>
2026-07-16 01:38:17 +0530

    Build landing page, auth UI and reusable components

--- frontend/eBookCreator/src/pages/LandingPage.jsx
+++ frontend/eBookCreator/src/pages/LandingPage.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import Navbar from '../components/layout/Navbar';
 import Hero from '../components/landing/Hero';
 import Features from "../components/landing/Features";
+import Testimonials from '../components/landing/Testimonials';
+import Footer from '../components/landing/Footer';
 
 const LandingPage = () => {
   return (
-    <div className="mb-[100vh]">
+    <div>
       <Navbar />
       <Hero />
       <Features />
+      <Testimonials />
+      <Footer />
+
     </div>
   )
 }
